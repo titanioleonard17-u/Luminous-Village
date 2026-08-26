8d847b43740ed9bea0c1e1075fecac4e5d20f919
EnokhIovan <jacobjovan26@gmail.com>
2026-08-26 21:16:50 +0700

    Setup Homescreen

---
+++ UI/Scripts/Button.gd
@@ -0,0 +1,24 @@
+extends PanelContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Label.text = name.trim_suffix('Button')
+	$TextButton.pressed.connect(__OnButtonPressed)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func __OnButtonPressed():
+	var action = name.trim_suffix("Button")
+	match action.to_lower():
+		"start":
+			print("Start")
+		"options":
+			print("Options")
+		"exit":
+			print("Exit")
+			get_tree().quit()
+		_:
+			print("Aksi '"+action+"' tidak terdaftar")
