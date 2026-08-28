28164ffd7d9c2889738e6bfdaa7ed39fa67db3e9
titan <titanioleonard17@gmail.com>
2026-08-28 20:20:28 +0700

    Update for Door Mecahnic (harus ke trigger terus menerus)

--- Script/Glass_Body.gd
+++ Script/Glass_Body.gd
@@ -3,14 +3,13 @@ extends RigidBody3D
 @export var torque_strength: float = 2.0
 @export var door_path: NodePath
 
-var activated: bool = false
+var is_hit_this_frame: bool = false
 
 func _ready():
 	gravity_scale = 0.0
 	angular_damp = 6.0
 	add_to_group("reflector")
 	add_to_group("laser_switch")
-
 	axis_lock_angular_x = true
 	axis_lock_angular_z = true
 
@@ -19,9 +18,12 @@ func handle_drag(delta: Vector2):
 	apply_torque_impulse(global_transform.basis * torque)
 
 func activate():
-	if activated:
-		return
-	activated = true
+	is_hit_this_frame = true
 	var door = get_node(door_path)
-	door.open_door()
-	print("Tombol kaca teraktivasi! Pintu terbuka.")
+	door.set_door_state(true)
+
+func _physics_process(_delta):
+	if not is_hit_this_frame:
+		var door = get_node(door_path)
+		door.set_door_state(false)
+	is_hit_this_frame = false  # reset untuk frame berikutnya
