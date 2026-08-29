59a3f699a683cb0befca128137b7fbca38403569
EnokhIovan <jacobjovan26@gmail.com>
2026-08-29 20:47:32 +0700

    Import BGM and setup

--- Asset/Sound/Scripts/AudioManager.gd
+++ Asset/Sound/Scripts/AudioManager.gd
@@ -1,31 +1,53 @@
 extends Node
 
-#var SFX = {
-	#"hoverSFX": preload("res://Asset/Sound/SFX/Hover.wav"),
-	#"clickDefaultSFX": preload("res://Asset/Sound/SFX/ClickButton.wav"),
-	#"clickOpenSFX": preload("res://Asset/Sound/SFX/ClickOpen.wav"),
-	#"clickCloseSFX": preload("res://Asset/Sound/SFX/ClickClose.wav")
-#}
-var regex := RegEx.new()
+var bgm_player: AudioStreamPlayer
+var sfx_player: AudioStreamPlayer
 
-var audio_player: AudioStreamPlayer
+var is_vibe_playing := false
+
+enum AudioType {
+	SFX,
+	BGM,
+	RANDOM_VIBE
+}
 
 func _ready() -> void:
-	audio_player = AudioStreamPlayer.new()
-	add_child(audio_player)
+	bgm_player = AudioStreamPlayer.new()
+	add_child(bgm_player)
 
+	sfx_player = AudioStreamPlayer.new()
+	add_child(sfx_player)
 
-func playSFX(sfx: String) -> void:
-	var path := find_sound(sfx)
+	bgm_player.finished.connect(_on_bgm_finished)
+
+func playAudio(audio_name: String, type: AudioType, random := false) -> void:
+	var path := ""
+
+	match type:
+		AudioType.SFX:
+			path = find_sound(audio_name)
+
+		AudioType.BGM:
+			path = find_bgm(audio_name)
+			is_vibe_playing = false
+
+		AudioType.RANDOM_VIBE:
+			path = get_random_vibe()
+			is_vibe_playing = true
 
 	if path == "":
-		push_error("SFX tidak ditemukan: " + sfx)
 		return
 
-	audio_player.stream = load(path)
-	audio_player.volume_db = get_sound_volume(path)
-	audio_player.play()
+	var player := sfx_player if type == AudioType.SFX else bgm_player
 
+	player.stream = load(path)
+	player.volume_db = get_sound_volume(path)
+	player.play()
+
+
+# =========================================================
+# SFX
+# =========================================================
 func find_sound(sound_name: String) -> String:
 	var path := "res://Asset/Sound/SFX"
 	var dir := DirAccess.open(path)
@@ -35,7 +57,7 @@ func find_sound(sound_name: String) -> String:
 		return ""
 
 	var regex := RegEx.new()
-	regex.compile("^([^_]+)_-?\\d+dB$")
+	regex.compile("^(.+)_-?\\d+(?:\\.\\d+)?dB$")
 
 	for file in dir.get_files():
 		var base_name := file.get_basename()
@@ -50,16 +72,103 @@ func find_sound(sound_name: String) -> String:
 	push_error("Sound tidak ditemukan: " + sound_name)
 	return ""
 
+
+# =========================================================
+# BGM
+# =========================================================
+func playRandomVibe() -> void:
+	var path := get_random_vibe()
+
+	if path == "":
+		push_error("Tidak ada BGM Vibe!")
+		return
+
+	is_vibe_playing = true
+
+	bgm_player.stream = load(path)
+	bgm_player.volume_db = get_sound_volume(path)
+	bgm_player.play()
+
+
+func stopBGM() -> void:
+	is_vibe_playing = false
+	bgm_player.stop()
+
+
+func find_bgm(bgm_name: String) -> String:
+	var path := "res://Asset/Sound/BGM"
+	var dir := DirAccess.open(path)
+
+	if dir == null:
+		push_error("Folder BGM tidak ditemukan: " + path)
+		return ""
+
+	var regex := RegEx.new()
+	regex.compile("^(.+)_-?\\d+(?:\\.\\d+)?dB$")
+
+	for file in dir.get_files():
+		var base_name := file.get_basename()
+		var result := regex.search(base_name)
+
+		if result:
+			var extracted_name := result.get_string(1)
+
+			if extracted_name == bgm_name:
+				return path + "/" + file
+
+	push_error("BGM tidak ditemukan: " + bgm_name)
+	return ""
+
+
+func get_random_vibe() -> String:
+	var path := "res://Asset/Sound/BGM"
+	var dir := DirAccess.open(path)
+
+	if dir == null:
+		push_error("Folder BGM tidak ditemukan: " + path)
+		return ""
+
+	var vibe_songs: Array[String] = []
+
+	var regex := RegEx.new()
+	regex.compile("^.+Vibe_-?\\d+(?:\\.\\d+)?dB$")
+
+	for file in dir.get_files():
+		var base_name := file.get_basename()
+
+		if regex.search(base_name):
+			vibe_songs.append(path + "/" + file)
+
+	if vibe_songs.is_empty():
+		push_error("Tidak ada BGM Vibe!")
+		return ""
+
+	return vibe_songs.pick_random()
+
+
+# =========================================================
+# VOLUME
+# =========================================================
+
 func get_sound_volume(path: String) -> float:
 	var file_name := path.get_file().get_basename()
 
 	var regex := RegEx.new()
-	regex.compile("_([+-]?\\d+)dB$")
+	regex.compile("_([+-]?\\d+(?:\\.\\d+)?)dB$")
 
 	var result := regex.search(file_name)
 
 	if result == null:
-		push_error("Format volume SFX tidak valid: " + file_name)
+		push_error("Format volume audio tidak valid: " + file_name)
 		return 0.0
 
 	return float(result.get_string(1))
+
+
+# =========================================================
+# BGM FINISHED
+# =========================================================
+
+func _on_bgm_finished() -> void:
+	if is_vibe_playing:
+		playRandomVibe()
