97f039297f7b4763c9539df93177f85955cd6ecc
EnokhIovan <jacobjovan26@gmail.com>
2026-09-02 20:37:34 +0700

    Editing options scene and transition scene

--- UI/Scripts/Transition.gd
+++ UI/Scripts/Transition.gd
@@ -1,18 +1,26 @@
 extends CanvasLayer
 
+var is_transitioning := false
+
 func play(location: String) -> void:
+	if is_transitioning:
+		return
+	
+	is_transitioning = true
 	self.visible = true
 	
-	# Tutup layar
+	$Blocker.mouse_filter = Control.MOUSE_FILTER_STOP
+	
 	$AnimationPlayer.play("Transition")
 	await $AnimationPlayer.animation_finished
 	
-	# Ganti scene saat layar sudah tertutup
 	get_tree().change_scene_to_file(location)
 	await get_tree().create_timer(0.5).timeout
 	
-	# Buka layar
 	$AnimationPlayer.play_backwards("Transition")
 	await $AnimationPlayer.animation_finished
 	
+	$Blocker.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
 	self.visible = false
+	is_transitioning = false
